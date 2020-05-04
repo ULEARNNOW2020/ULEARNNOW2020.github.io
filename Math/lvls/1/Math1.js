47c4b23e776efcdd47d7f7ad5dbecf1db4a586c7
LOLsite <55099005+LOLsite@users.noreply.github.com>
2020-05-05 04:05:04 +0700

    fruits

--- Math/lvls/1/Math1.js
+++ Math/lvls/1/Math1.js
@@ -14,6 +14,10 @@ question.src = "img/question.png";
 const bg = new Image();
 bg.src = "img/bg.png";
 
+const fr1 = new Image();
+const fr2 = new Image();
+
+
     //  РАНДОМ ЧИСЕЛ
 function getRandom(min, max) {
   return Math.random() * (max - min) + min;
@@ -21,7 +25,11 @@ function getRandom(min, max) {
 
 var first1 = getRandom(0, 10);
 var second1 = getRandom(0, 10);
+var frut1 = getRandom(0, 8);
+var frut21 = getRandom(0, 8);
 
+var frut = Math.round(frut1);
+var frut2 = Math.round(frut21);
 var first = Math.round(first1);
 var second = Math.round(second1);
 
@@ -29,9 +37,11 @@ var second = Math.round(second1);
 
    //    ОТРИСОВКА
 function draw(){
-  ctx.fillStyle = "black";
   ctx.clearRect(0,0,1500,500);
   ctx.drawImage(bg,0,0,3000,3000);
+  ctx.drawImage(fr1,367,50,160,170);
+  ctx.drawImage(fr2,760,50,160,170);
+  ctx.fillStyle = "black";
   ctx.font = "50px Arial";
   ctx.fillText(first,430,150,90);
   ctx.fillText(second,820,150,90);
@@ -51,12 +61,47 @@ function draw(){
   ctx.fillText("вы не успели",500,200,300);
 }
 
-
  if(time<0){
    time=0;
  }
  if (time==0){
    ans=3;
+ }
+
+ if (frut == 0){
+   fr1.src = "img/fruits/app.png";
+ } else if (frut == 1){
+   fr1.src = "img/fruits/bak.png";
+ } else if (frut == 2){
+   fr1.src = "img/fruits/banan.png";
+ } else if (frut == 3){
+   fr1.src = "img/fruits/grush.png";
+ } else if (frut == 4){
+   fr1.src = "img/fruits/klub.png";
+ } else if (frut == 5){
+   fr1.src = "img/fruits/orang.png";
+ } else if (frut == 6){
+   fr1.src = "img/fruits/pom.png";
+ } else if (frut == 7){
+   fr1.src = "img/fruits/vin.png";
+ }
+
+ if (frut2 == 0){
+   fr2.src = "img/fruits/app.png";
+ } else if (frut2 == 1){
+   fr2.src = "img/fruits/bak.png";
+ } else if (frut2 == 2){
+   fr2.src = "img/fruits/banan.png";
+ } else if (frut2 == 3){
+   fr2.src = "img/fruits/grush.png";
+ } else if (frut2 == 4){
+   fr2.src = "img/fruits/klub.png";
+ } else if (frut2 == 5){
+   fr2.src = "img/fruits/orang.png";
+ } else if (frut2 == 6){
+   fr2.src = "img/fruits/pom.png";
+ } else if (frut2 == 7){
+   fr2.src = "img/fruits/vin.png";
  }
 }
 
@@ -77,6 +122,11 @@ function restart(){
   }
   first = Math.round(first1);
   second = Math.round(second1);
+
+  frut1 = getRandom(0, 8);
+  frut21 = getRandom(0, 8);
+  frut = Math.round(frut1);
+  frut2 = Math.round(frut21);
 }
 
     //   ОТВЕТЫ
